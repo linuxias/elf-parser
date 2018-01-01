f5c54b0ce0e2e8918ecb824f6760a188f5f6461c
Seungha Son <seungha.son@samsung.com>
2018-01-01 23:28:04 +0900

    Add logic to get process info

---
+++ main.c
@@ -0,0 +1,39 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdint.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <unistd.h>
+#include <fctrl.h>
+
+int main(int argc, char *argv[])
+{
+	int ret;
+	int fd;
+	int i;
+	uint8_t *mem;
+	struct stat st;
+	char *string_table;
+	char *interp;
+
+	if (argc < 2) {
+		printf("Usage: %s <executable>\n", argv[0]);
+		exit(0);
+	}
+
+	errno = 0;
+	fd = open(argv[1], O_RDONLY);
+	if (errno < 0 || fd == -1) {
+		perror("open %d", errno);
+		exit(-1);
+	}
+
+	errno = 0;
+	ret = fstat(fd, &st);
+	if (ret < 0) {
+		perror("fstat %d", errno);
+		exit(-1);
+	}
+
+	return 0;
+}
